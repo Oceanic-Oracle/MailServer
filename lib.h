18038d04b8e0e300cb0c08264ff6c9acb798ed63
Oceanic-Oracle <146174115+Oceanic-Oracle@users.noreply.github.com>
2024-03-23 19:58:38 +0300

    start

---
+++ lib.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include "lib.h"
+
+int main(int argc, char* argv[])
+{
+    WSAData wsaData;
+    WORD DLLVersion = MAKEWORD(2, 1);
+
+    if (WSAStartup(DLLVersion, &wsaData) != 0)
+    {
+        std::cout << "Error WSAStartup" << std::endl;
+        return 1;
+    }
+
+    SOCKADDR_IN addr;
+    int sizeaddr = sizeof(addr);
+    inet_pton(AF_INET, "127.0.0.1", &(addr.sin_addr));
+    addr.sin_port = htons(1234);
+    addr.sin_family = AF_INET;
+
+    SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+    if (sListen == INVALID_SOCKET)
+    {
+        std::cout << "Error socket creation" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    if (bind(sListen, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR)
+    {
+        std::cout << "Error bind" << std::endl;
+        closesocket(sListen);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(sListen, SOMAXCONN) == SOCKET_ERROR)
+    {
+        std::cout << "Error listen" << std::endl;
+        closesocket(sListen);
+        WSACleanup();
+        return 1;
+    }
+
+    SOCKET newConnection;
+    SOCKADDR_IN clientAddr;
+    int clientAddrSize = sizeof(clientAddr);
+    newConnection = accept(sListen, (SOCKADDR*)&clientAddr, &clientAddrSize);
+    if (newConnection == INVALID_SOCKET)
+    {
+        std::cout << "Error accept" << std::endl;
+        closesocket(sListen);
+        WSACleanup();
+        return 1;
+    }
+    else
+    {
+        std::cout << "Client Connected" << std::endl;
+    }
+
+    // Закрываем сокеты и очищаем Winsock
+    closesocket(newConnection);
+    closesocket(sListen);
+    WSACleanup();
+
+    return 0;
+}
